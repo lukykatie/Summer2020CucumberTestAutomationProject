41f87089f8f1b0648961334c19783f27ee6a7962
lukykatie <lukykatie@gmail.com>
2020-11-10 21:08:19 -0500

    New update

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "rerun:target/rerun.txt"
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
@@ -17,11 +18,11 @@ import org.junit.runner.RunWith;
         publish = true
 )
 public class CucumberRunner {
-    //tags = "@s_o and @with_two_columns",
-    // scenario must have BOTH tags
-    //java logic if(@s_o && @with_two_columns)
+        //tags = "@s_o and @with_two_columns",
+        // scenario must have BOTH tags
+        //java logic if(@s_o && @with_two_columns)
 
-    //tags = "@s_o or @with_two_columns",
-    // scenario must have EITHER OR tags
-    //java logic if(@s_o || @with_two_columns),
+        //tags = "@s_o or @with_two_columns",
+        // scenario must have EITHER OR tags
+        //java logic if(@s_o || @with_two_columns),
 }
